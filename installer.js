e28e73a053108d7335de73d245c5b7bbcea40668
myTerminal <team.terminal@gmail.com>
2014-07-22 20:44:34 +0530

    Added package-script as dependency, added grunt-cli and npm-check-updates as global dependencies

---
+++ installer.js
@@ -0,0 +1,10 @@
+require('package-script').spawn([
+    {
+	command: "npm",
+	args: ["install", "-g", "grunt-cli"]
+    },
+    {
+        command: "npm",
+        args: ["install", "-g", "npm-check-updates"]
+    }
+]);
