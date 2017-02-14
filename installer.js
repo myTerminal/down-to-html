4aaee1e91a3ca029fd130a146058ae55bd734fbc
myTerminal <team.terminal@gmail.com>
2017-02-14 09:57:17 +0530

    remove ncu from installer.js

--- installer.js
+++ installer.js
@@ -2,9 +2,5 @@ require('package-script').spawn([
     {
 	command: "npm",
 	args: ["install", "-g", "grunt-cli"]
-    },
-    {
-        command: "npm",
-        args: ["install", "-g", "npm-check-updates"]
     }
 ]);
